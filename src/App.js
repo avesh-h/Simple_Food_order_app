da829cc17b718d2ab9368758b455b61c524385c5
Avesh <aveshhasanfatta1155@gmail.com>
2022-09-11 20:07:01 +0530

    add cart in firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Carts from './components/Cart/Carts';
 import Navbar from './components/Layout/Navbar';
 import Products from './components/Product/Products';
 import {useDispatch,useSelector} from "react-redux"
+import {useEffect} from 'react'
 
 
 function App() {
@@ -11,6 +12,22 @@ function App() {
   const showCart = useSelector((state)=>{
     return state.ui.cartVisible
   })
+
+  const cart = useSelector((state)=>{
+    return state.cart
+  })
+
+  useEffect(()=>{
+    const sendCartData = async ()=>{
+      const response = await fetch('https://food-cart-app-3e126-default-rtdb.firebaseio.com/cart.json',{
+        method : 'PUT',
+        body : JSON.stringify(cart)
+      })     
+    }
+    sendCartData().catch((err)=>{
+      console.log(err)
+    })
+  },[cart])
 
 
   return (
